2b338421283724170092b7c444f1038e65620ce9
HeyItsBradley <bradleyrodriguez77@gmail.com>
2022-11-09 14:46:29 -0700

    added parktext and parkindex to be able to pull the individual park codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var parkCode = "";
+var allParkCodes = [];
+var parksInState = [];
+
 $(document).ready(function () {
   $(".selectState li").on("click", function () {
     //delete all lis in select park ul
@@ -19,24 +23,44 @@ $(document).ready(function () {
         return response.json();
       })
       .then(function (data) {
+        console.log(data);
         for (var i = 0; i < data.total; i++) {
-          console.log(data);
-
           var createLi = document.createElement("li");
           var createA = document.createElement("a");
           var selectPark = document.getElementById("parkUl");
           createA.setAttribute("class", "dropdown-item");
           createA.setAttribute("href", "#");
+          createA.setAttribute("id", "parkIndex" + [i]);
 
           createA.textContent = data.data[i].name;
           createLi.appendChild(createA);
           selectPark.appendChild(createLi);
+
+          allParkCodes.push(data.data[i].parkCode);
+          parksInState.push(data.data[i].name);
         }
+        console.log(allParkCodes);
+        console.log(parksInState);
+
         $(".selectPark a").on("click", function () {
           console.log("second click is working");
           $("#activitiesUl").empty();
-          console.log($(this).text());
           $("#parkInput").val($(this).text());
+          var parkText = this.id;
+          console.log(parkText);
+          var parkIndex = parkText.replace(/\D/g, "");
+          console.log(parkIndex);
+          fetch(
+            "https://developer.nps.gov/api/v1/thingstodo?stateCode=" +
+              parkcode +
+              "&api_key=b295jCwiBszLNsrrMo1lhHZLgvlqvNzbudqN6gHc"
+          )
+            .then(function (response) {
+              return response.json();
+            })
+            .then(function (data) {
+              console.log(data);
+            });
         });
       });
   });
